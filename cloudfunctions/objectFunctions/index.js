05a8c7619ccc8dba022430d281613cbd05aa2a60
robinllb <robin_llb@163.com>
2022-10-01 13:22:39 +0800

    user

--- cloudfunctions/objectFunctions/index.js
+++ cloudfunctions/objectFunctions/index.js
@@ -1,5 +1,8 @@
 const getObjectList = require('./getObjectList/index');
 const getOpenId = require('./getOpenId/index')
+const addUser = require('./addUser/index')
+const updateUser = require('./updateUser/index')
+const selectUser = require('./selectUser/index')
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -8,5 +11,11 @@ exports.main = async (event, context) => {
       return await getObjectList.main(event, context);
     case 'getOpenId':
       return await getOpenId.main(event, context);
+    case 'addUser':
+      return await addUser.main(event,context);
+    case 'updateUser':
+      return await updateUser.main(event,context);
+    case 'selectUser':
+      return await selectUser.main(event,context);
   }
 };
